d7851b1b61a0c087944d76183ae3517187d426a0
Denis <todenis@inbox.ru>
2020-02-01 15:56:23 +0300

    Добавил решение домашнего задания по занятию №5

--- lesson05/js/script.js
+++ lesson05/js/script.js
@@ -1,303 +1,218 @@
 'use strict';
 
 
-function task01() {
+let PRODUCTS_NAMES = ['Processor', 'Display', 'Notebook', 'Mouse', 'Keyboard'];
+let PRICES = [100, 120, 1000, 15, 18];
+let IDS = [0, 1, 2, 3, 4];
+let IMG = ['https://www.placehold.it/200x150', 'https://www.placehold.it/200x150', 'https://www.placehold.it/200x150',
+  'https://www.placehold.it/200x150', 'https://www.placehold.it/200x150'
+];
+let BASKETIMG = ['https://www.placehold.it/100x80', 'https://www.placehold.it/100x80', 'https://www.placehold.it/100x80',
+  'https://www.placehold.it/100x80', 'https://www.placehold.it/100x80'
+];
 
-  class Converter {
-    constructor(hundreds, dozens, units) {
-      this['сотни'] = hundreds;
-      this['десятки'] = dozens;
-      this['единицы'] = units;
-    }
+
+
+function createProductsDTO(arr) {
+  arr = [];
+
+  for (let i = 0; i < IDS.length; i++) {
+    arr.push(createProduct(i));
   }
-
-  /**
-   *@description функция проверяет корректность ввода пользователя
-   *
-   * @param {string} num число в виде строки, полученное от пользователя. Должно быть от 0 до 999
-   * @returns {boolean} true - если ввод корректный. False - если ввод некорректный
-   */
-  function isСorrectNum(num) {
-    if (+num > 0 && +num < 999 && !isNaN(+num) && num != '' && num != null && num.length < 4) {
-      return true;
-    } else {
-      console.log('Введенное число некорректно');
-      return false;
-    }
-  }
-
-  /**
-   *@description функция конвертирует число в объект по разрядам сотни = , десятки = , единицы =
-   *
-   * @param {string} num число в виде строки, полученное от пользователя. Должн быть от 0 до 999
-   * @returns {object} возвращает объект с полученными разрядами
-   */
-  function convertNum(num) {
-    (num.length == 2) && (num = '0' + num);
-    (num.length == 1) && (num = '00' + num);
-    let obj = new Converter(...num);
-    console.log(obj);
-    return obj;
-  }
-
-  /**
-   *@description запускает процесс проверки введенного числа и конвертации в объект
-   *
-   * @param {string} num - число в виде строки, полученное от пользователя. Должн быть от 0 до 999
-   * @returns {object} - возвращает конвертированный объект или пустой объект, если введенное число некорректно
-   */
-  function startConvertation(num) {
-    return isСorrectNum(num) ? convertNum(num) : {};
-  }
-
-  let inputNumber = prompt('Введите число от 0 до 999', '');
-  startConvertation(inputNumber);
+  return arr;
 }
 
+function createProduct(index) {
+  return {
+    product_name: PRODUCTS_NAMES[index],
+    price: PRICES[index],
+    id_product: IDS[index],
+    product_img: IMG[index],
+    product_basket_img: BASKETIMG[index]
+  };
+}
+// конец исходных данных
 
+let basketDiv = document.getElementById('basket');
+let basketBtn = document.getElementById('basketBtn');
+let mainCatalog = document.querySelector('#catalog');
+let buyBtns;
+let removeBtns;
 
-function task02() {
-  // исходные данные
-  let PRODUCTS_NAMES = ['Processor', 'Display', 'Notebook', 'Mouse', 'Keyboard'];
-  let PRICES = [100, 120, 1000, 15, 18];
-  let IDS = [0, 1, 2, 3, 4];
-
-  let products = [];
-
-  function createProductsDTO() {
-    let length = IDS.length;
-
-    for (let i = 0; i < length; i++) {
-      products.push(createProduct(i));
-    }
-  }
-
-  function createProduct(index) {
-    return {
-      product_name: PRODUCTS_NAMES[index],
-      price: PRICES[index],
-      id_product: IDS[index]
-    };
-  }
-  // конец исходных данных
-
-  /**
-   *@description Функция создает пустую корзину в виде пустого массива
-   *
-   * @returns {array} возвращает пустой массив
-   */
-  function createBasket() {
-    let basket = [];
-    return basket;
-  }
-
-  /**
-   *@description добавляет продукт созданный в createProductBasket в корзину
-   *
-   * @param {array} basket корзина, представленная в виде массива
-   * @param {number} index индекс продукта в исходном массиве products
-   * @param {number} quantity количество единиц добавляемого продукта
-   * @returns {array} возвращает корзину, в виде массива с добавленным объектом продукта
-   */
-  function addProductToBasket(basket, index, quantity) {
-    let chosenProduct = createProductBasket(index, quantity);
-    basket.push(chosenProduct);
-    return basket;
-  }
-
-  /**
-   *@description создает объект выбранного продукта, который будет помещен в корзину
-   *
-   * @param {number} index индекс продукта в исходном массиве products
-   * @param {number} quantity количество единиц добавляемого продукта
-   * @returns {object} возвращает обект продукта, готовый для добавления в корзину
-   */
-  function createProductBasket(index, quantity) {
-    return {
-      product: products[index].product_name,
-      price: products[index].price,
-      id: products[index].id_product,
-      quantity: quantity
-    };
-  }
-
-  /**
-   *@description удаляет выбранный продукт из корзины
-   *
-   * @param {array} basket корзина в виде массива продуктов (объектов)
-   * @param {number} basketIndex - индекс удаляемого продукта в массиве (корзине)
-   * @returns {array} корзину продуктов в виде массива без удаленного продукта
-   */
-  function removeProductFromBasket(basket, basketIndex) {
-    basket.splice(basketIndex, 1);
-    return basket;
-  }
-
-  /**
-   *@description считает общую стоимость продуктов в корзине, учитывая их количество
-   *
-   * @param {array} basket - корзина в виде массива продуктов (объектов)
-   * @returns {number} возвращает общую стоимость продуктов в корзине
-   */
-  function countBasketPrice(basket) {
-    let basketCost = 0;
-    basket.forEach(function(product) {
-      let elemCost = product.price * product.quantity;
-      basketCost += elemCost;
+let catalog = {
+  items: [],
+  message: 'Ваша корзина пуста',
+  loadProducts() {
+    this.items = createProductsDTO();
+  },
+  createCatalog() {
+    this.items.forEach(function(elem) {
+      //create
+      let product = document.createElement('div');
+      let productImg = document.createElement('img');
+      let productInfo = document.createElement('div');
+      let productHeader = document.createElement('h3');
+      let productPrice = document.createElement('p');
+      let buyBtn = document.createElement('button');
+      //customize
+      product.classList.add('product');
+      productImg.classList.add('productImg');
+      productInfo.classList.add('productInfo');
+      productHeader.classList.add('productHeader');
+      productPrice.classList.add('productPrice');
+      buyBtn.classList.add('buyBtn');
+      productImg.src = elem.product_img;
+      productImg.alt = 'Product Image';
+      productImg.title = 'Product Image';
+      productHeader.textContent = elem.product_name;
+      productPrice.textContent = elem.price;
+      buyBtn.textContent = 'Купить';
+      //append
+      mainCatalog.appendChild(product);
+      product.appendChild(productImg);
+      product.appendChild(productInfo);
+      productInfo.appendChild(productHeader);
+      productInfo.appendChild(productPrice);
+      productInfo.appendChild(buyBtn);
     });
-    console.log(`Общая стоиомсть выбранных продуктов в корзине : ${basketCost}`);
-    return basketCost;
   }
+};
 
-  createProductsDTO();
+let basket = {
+  items: [],
+  addProduct(n) {
+    let chosenProduct = {};
+    Object.assign(chosenProduct, catalog.items[n]);
+    let choosedId = chosenProduct.id_product;
+    let isNewProduct = true;
+    this.items.forEach((elem) => {
+      if (elem.id_product == choosedId) {
+        elem.quantity++;
+        elem.cost = elem.price * elem.quantity;
+        isNewProduct = false;
+      }
+    });
 
-  console.log(`Список доступных продуктов:`);
-  console.log(products);
-
-  let basket = createBasket();
-
-  console.log('Добавляем товары в корзину');
-  basket = addProductToBasket(basket, 0, 2);
-  basket = addProductToBasket(basket, 3, 2);
-  basket = addProductToBasket(basket, 2, 5);
-
-
-  console.log(`Ваша корзина: `);
-  console.log(basket);
-
-  console.log('Подсчитываем общую стоимость товаров в корзине');
-  countBasketPrice(basket);
-}
-
-
-//////// Дальше Быки и коровы/////////
-
-/**
- * @description Запускает игру, запускает остальные функции. Функции ввода предположения и сравнения его с загаданным числом зациклены бесконечно, до момента угадывания загаданного числа
- */
-function startGame() {
-  getRules();
-  let secretNum = getNumber();
-  console.log(secretNum);  // Показываю загаданное число для проверки игры
-  let counter = 0;
-  while (true) {
-    counter++;
-    if (startRound(secretNum)) {
-      break;
+    if (isNewProduct) {
+      chosenProduct.quantity = 1;
+      this.items.push(chosenProduct);
+      this.items[this.items.length - 1].cost = this.items[this.items.length - 1].price * this.items[this.items.length - 1].quantity;
     }
+  },
+  removeProduct(n) {
+    this.items.splice(n, 1);
+  },
+  getTotalCost() {
+    this.totalCost = 0;
+    this.items.forEach((elem) => this.totalCost += elem.cost);
+  },
+  getTotalAmount() {
+    this.totalAmount = 0;
+    this.items.forEach((elem) => this.totalAmount += elem.quantity);
+  },
+  showBasket() {
+
+    if (this.items.length == 0) {
+      this.clearBasket();
+      this.message = `Ваша корзина пуста`;
+      this.showMessage(this.message);
+      return;
+    } else {
+      this.clearBasket();
+      this.message = `В корзине ${this.totalAmount} товаров на сумму ${this.totalCost} рублей`;
+      this.showMessage(this.message);
+    }
+
+    this.items.forEach((elem) => {
+
+      //create
+      let basketProduct = document.createElement('div');
+      let basketProductImg = document.createElement('img');
+      let basketProductHeader = document.createElement('h3');
+      let basketProductPrice = document.createElement('p');
+      let basketProductQuantityWrap = document.createElement('div');
+      let basketProductQuantity = document.createElement('p');
+      let basketProductQuantityDetail = document.createElement('p');
+      let basketRemoveBtn = document.createElement('button');
+
+      //append
+      basketProduct.classList.add('basketProduct');
+      basketProductImg.classList.add('basketProductImg');
+      basketProductImg.src = elem.product_basket_img;
+      basketProductHeader.classList.add('basketProductHeader');
+      basketProductHeader.textContent = elem.product_name;
+      basketProductPrice.classList.add('basketProductPrice');
+      basketProductPrice.textContent = elem.cost;
+      basketProductQuantityWrap.classList.add('basketProductQuantityWrap');
+      basketProductQuantity.textContent = `Quantity: ${elem.quantity}`;
+      basketProductQuantityDetail.classList.add('details');
+      basketProductQuantityDetail.textContent = `${elem.price} each`;
+      basketRemoveBtn.classList.add('basketRemoveBtn');
+      basketRemoveBtn.textContent = 'X';
+
+      // customize
+      basketDiv.appendChild(basketProduct);
+      basketProduct.appendChild(basketProductImg);
+      basketProduct.appendChild(basketProductHeader);
+      basketProduct.appendChild(basketProductPrice);
+      basketProduct.appendChild(basketProductQuantityWrap);
+      basketProductQuantityWrap.appendChild(basketProductQuantity);
+      basketProductQuantityWrap.appendChild(basketProductQuantityDetail);
+      basketProduct.appendChild(basketRemoveBtn);
+    });
+  },
+  clearBasket() {
+    while (basketDiv.firstChild) {
+      basketDiv.removeChild(basketDiv.firstChild);
+    }
+  },
+  showMessage(text) {
+    let messageElement = document.createElement('div');
+    messageElement.textContent = text;
+    basketDiv.appendChild(messageElement);
   }
-  console.log(`Поздравляю! Загаданное число - ${secretNum}. Число попыток: ${counter}`);
-}
+};
 
-/**
- * @description Описывает правила игры в консоли
- */
-function getRules() {
-  console.log('Я загадала 4 неповторяющиеся цифры. Тебе нужно их отгадать в порядке, в котором я их загадала');
-  console.log('Я буду тебе давать подсказки после каждого предположения');
-  console.log('Количество коров - когда ты угадываешь цифру, но не ее позицию');
-  console.log('количество быков - когда ты угадываешь и цифру и ее позицию. Удачи!');
-}
 
-/**
- * @description Генерирует случайное четырехзначное число, из неповторяющихся цифр
- * @returns {string} Возвращает четырехзначное число в виде строки, которое в дальнейшем нужно угадать
- */
-function getNumber() {
-  let secretNum = '';
-  for (let i = 0; i < 4; i++) {
-    let digit = getRandomDigit();
-    checkDigit(digit, secretNum) ? i-- : secretNum += digit;
+
+window.addEventListener('DOMContentLoaded', function() {
+  catalog.loadProducts();
+  mainCatalog = catalog.createCatalog();
+  basket.showBasket();
+  basketBtn.addEventListener('click', function() {
+    basketDiv.classList.toggle('hidden');
+  });
+  buyBtns = document.querySelectorAll('.buyBtn');
+});
+
+
+
+mainCatalog.addEventListener('click', function(e) {
+  let target = e.target;
+  if (target && target.classList.contains('buyBtn')) {
+    buyBtns.forEach((elem, i) => {
+      if (target == elem) {
+        basket.addProduct(i);
+        basket.getTotalCost();
+        basket.getTotalAmount();
+        basket.showBasket();
+        removeBtns = document.querySelectorAll('.basketRemoveBtn');
+      }
+    });
   }
-  return secretNum;
-}
+});
 
-/**
- *@description Генерирует случайное число от 0 до 10, не включая 10
- *
- * @returns {number} возвращает число от 0 до 10, не включая 10
- */
-function getRandomDigit() {
-  return Math.floor(Math.random() * 10);
-}
-
-/**
- *@description проверяет содержится ли число в строке
- *
- * @param {number} num - проверяемое число
- * @param {string} str - проверяемая строка
- * @returns {boolean} возвращает true, если число в строке содаржится, иначе возвращает false
- */
-function checkDigit(num, str) {
-  return (str.includes(num) ? true : false);
-}
-
-/**
- * @description Позволяет пользователю ввести свое предположение о загаданном числе при помощи promt
- * @returns {string} Возвращает число в виде строки, введенное пользователем
- */
-function makeGuess() {
-  let guess = prompt('Сделайте свое предположение');
-  return guess;
-}
-
-/**
- *@description функция заускает один раунд игры
- *
- * @param {string} secret - загаданное число
- * @returns возвращает true только если пользователь угадал загаданное число (пришло true из makeCompartion)
- */
-function startRound(secret) {
-  let guess = makeGuess();
-  if (makeCompartion(guess, secret)) {
-    return true;
+basketDiv.addEventListener('click', function(e) {
+  let target = e.target;
+  if (target && target.classList.contains('basketRemoveBtn')) {
+    removeBtns.forEach((elem, i) => {
+      if (target == elem) {
+        basket.removeProduct(i);
+        basket.getTotalCost();
+        basket.getTotalAmount();
+        basket.showBasket();
+        removeBtns = document.querySelectorAll('.basketRemoveBtn');
+      }
+    });
   }
-}
-
-/**
- * @description Сравнивает цифры из загаданного числа и числа введенного пользователем, считает количество быков и коров
- * @param {string} guess - предположенное пользователем число
- * @param {string} secret - загаданное число
- * @returns возвращает true только если пользователь угадал загаданное число
- */
-function makeCompartion(guess, secret) {
-  if (guess == secret) {
-    return true;
-  } else {
-    let bulls = searchBulls(guess, secret);
-    let cows = searchCows(guess, secret, bulls);
-  }
-}
-
-/**
- *@description функция считает число быков в предположенном варианте пользователя
- *
- * @param {string} guess - предположенное пользователем число
- * @param {string} secret - загаданное число
- * @returns {number} возвращает количество найденных быков
- */
-function searchBulls(guess, secret) {
-  let bulls = 0;
-  for (let i = 0; i < secret.length; i++) {
-    (guess[i] == secret[i]) && (bulls++);
-  }
-  console.log(`Быков: ${bulls}`);
-  return bulls;
-}
-
-/**
- *@description функция считает число коров в предположенном варианте пользователя
- *
- * @param {string} guess - предположенное пользователем число
- * @param {string} secret - загаданное число
- * @param {number} bulls - найденное количество быков в предположении пользователя
- * @returns {number} возвращает количество найденных быков
- */
-function searchCows(guess, secret, bulls) {
-  let cows = 0;
-  for (let i = 0; i < secret.length; i++) {
-    (secret.includes(guess[i])) && (cows++);
-  }
-  console.log(`Коров: ${cows - bulls}`);
-  return cows - bulls;
-}
+});
