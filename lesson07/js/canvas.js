7e2ad8ed61cc1bad7c69fae825255c4290698e01
Denis <todenis@inbox.ru>
2020-02-07 02:38:03 +0300

    Тстирую новые функции

--- lesson07/js/canvas.js
+++ lesson07/js/canvas.js
@@ -17,7 +17,7 @@ let editor = {
   y: 0,
 
   init() {
-    document.querySelector(this.container).addEventListener ('input', this.inputHandler);
+    document.querySelector(this.container).addEventListener('input', this.inputHandler);
     document.querySelector(this.container).addEventListener('click', this.clickHandler);
 
     canvas.addEventListener('mousemove', this.getCoordinates);
@@ -34,12 +34,20 @@ let editor = {
   clickHandler(evt) {
     if (evt.target.name === 'tool-button') {
       editor.currentTool = evt.target.dataset.name;
+    } else if (evt.target.name === 'save-button') {
+      editor.url = canvas.toDataURL();
+      console.log(editor.url);
     }
   },
   inputHandler(evt) {
     if (evt.target.name === 'input-obj') {
       editor[`current-${evt.target.dataset.name}`] = evt.target.value;
-      evt.target.dataset.name === 'color' ? ctx.fillStyle = editor['current-color'] : ctx.fillStyle = ctx.fillStyle;
+      if (evt.target.dataset.name === 'color') {
+        ctx.fillStyle = editor['current-color'];
+        ctx.strokeStyle = editor['current-color'];
+      } else {
+        ctx.fillStyle = ctx.fillStyle;
+      }
     }
   },
   startDraw() {
@@ -49,6 +57,8 @@ let editor = {
       editor.drawBrush();
     } else if (editor.currentTool === 'stroke') {
       editor.drawStroke();
+    } else if (editor.currentTool === 'spray') {
+      editor.drawSpray();
     }
   },
   endDraw() {
@@ -56,14 +66,14 @@ let editor = {
   },
   drawPencil() {
     canvas.onmousemove = () => {
-      ctx.fillRect (editor.x, editor.y, editor['current-size'], editor['current-size']);
+      ctx.fillRect(editor.x, editor.y, editor['current-size'], editor['current-size']);
     };
   },
   drawBrush() {
     canvas.onmousemove = () => {
       ctx.moveTo(editor.x, editor.y);
       ctx.beginPath();
-      ctx.ellipse (editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI/4, 0, 2*Math.PI);
+      ctx.ellipse(editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI / 4, 0, 2 * Math.PI);
       // ctx.stroke();
       ctx.fill();
       ctx.closePath();
@@ -73,12 +83,23 @@ let editor = {
     canvas.onmousemove = () => {
       ctx.moveTo(editor.x, editor.y);
       ctx.beginPath();
-      ctx.ellipse (editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI/4, 0, 2*Math.PI);
+      ctx.ellipse(editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI / 4, 0, 2 * Math.PI);
       ctx.stroke();
       // ctx.fill();
       ctx.closePath();
     };
-  }
+  },
+  drawSpray() {
+    canvas.onmousemove = () => {
+      ctx.fillRect(editor.x + 10, editor.y + 7, 1, 1);
+      ctx.fillRect(editor.x + 3, editor.y + 9, 1, 1);
+      ctx.fillRect(editor.x, editor.y, 1, 1);
+      ctx.fillRect(editor.x + 7, editor.y + 2, 1, 1);
+      ctx.fillRect(editor.x - 9, editor.y - 5, 1, 1);
+      ctx.fillRect(editor.x - 5, editor.y - 3, 1, 1);
+      ctx.fillRect(editor.x - 2, editor.y - 1, 1, 1);
+    };
+  },
 };
 
 editor.init();
