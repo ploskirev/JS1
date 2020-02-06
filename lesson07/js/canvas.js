0b8e84bfe1a5915ed541263e49688326717a72ae
Denis <todenis@inbox.ru>
2020-02-07 01:39:43 +0300

    Добавлены способы рисования круглой кистью и кругом без заливки

--- lesson07/js/canvas.js
+++ lesson07/js/canvas.js
@@ -16,7 +16,7 @@ let editor = {
   x: 0,
   y: 0,
 
-  _init() {
+  init() {
     document.querySelector(this.container).addEventListener ('input', this.inputHandler);
     document.querySelector(this.container).addEventListener('click', this.clickHandler);
 
@@ -43,18 +43,42 @@ let editor = {
     }
   },
   startDraw() {
-    if (editor.currentTool === 'brush') {
-      editor._drawPencil();
+    if (editor.currentTool === 'pencil') {
+      editor.drawPencil();
+    } else if (editor.currentTool === 'brush') {
+      editor.drawBrush();
+    } else if (editor.currentTool === 'stroke') {
+      editor.drawStroke();
     }
   },
   endDraw() {
     canvas.onmousemove = null;
   },
-  _drawPencil() {
+  drawPencil() {
     canvas.onmousemove = () => {
       ctx.fillRect (editor.x, editor.y, editor['current-size'], editor['current-size']);
+    };
+  },
+  drawBrush() {
+    canvas.onmousemove = () => {
+      ctx.moveTo(editor.x, editor.y);
+      ctx.beginPath();
+      ctx.ellipse (editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI/4, 0, 2*Math.PI);
+      // ctx.stroke();
+      ctx.fill();
+      ctx.closePath();
+    };
+  },
+  drawStroke() {
+    canvas.onmousemove = () => {
+      ctx.moveTo(editor.x, editor.y);
+      ctx.beginPath();
+      ctx.ellipse (editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI/4, 0, 2*Math.PI);
+      ctx.stroke();
+      // ctx.fill();
+      ctx.closePath();
     };
   }
 };
 
-editor._init();
+editor.init();
