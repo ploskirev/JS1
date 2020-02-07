ee2fba9d1b06238ad84a4f4da190a3f88ed0bbdf
Denis <todenis@inbox.ru>
2020-02-08 01:48:33 +0300

    Добавлены дополнительные инструменты, подключен сторонний colorPicker, создан интерфейс

--- lesson07/js/canvas.js
+++ lesson07/js/canvas.js
@@ -19,6 +19,7 @@ let editor = {
   init() {
     document.querySelector(this.container).addEventListener('input', this.inputHandler);
     document.querySelector(this.container).addEventListener('click', this.clickHandler);
+    document.querySelector('.clear').addEventListener('click', this.clearCanvas);
 
     canvas.addEventListener('mousemove', this.getCoordinates);
     canvas.addEventListener('mousedown', this.startDraw);
@@ -32,22 +33,31 @@ let editor = {
     yCoord.innerText = editor.y;
   },
   clickHandler(evt) {
+    let saveBtn = document.querySelector('#save-button');
     if (evt.target.name === 'tool-button') {
       editor.currentTool = evt.target.dataset.name;
+      let toolsBtns = document.querySelectorAll('button[name="tool-button"]');
+      for (let i = 0; i < toolsBtns.length; i++) {
+        toolsBtns[i].classList.remove('activeTool');
+        if (evt.target == toolsBtns[i]) {
+          toolsBtns[i].classList.add('activeTool');
+        }
+      }
     } else if (evt.target.name === 'save-button') {
-      editor.url = canvas.toDataURL();
-      console.log(editor.url);
+      editor.url = canvas.toDataURL('image/png');
+      saveBtn.href = editor.url;
     }
   },
   inputHandler(evt) {
     if (evt.target.name === 'input-obj') {
       editor[`current-${evt.target.dataset.name}`] = evt.target.value;
-      if (evt.target.dataset.name === 'color') {
-        ctx.fillStyle = editor['current-color'];
-        ctx.strokeStyle = editor['current-color'];
-      } else {
-        ctx.fillStyle = ctx.fillStyle;
-      }
+      editor['current-size'] = evt.target.value;
+      // if (evt.target.dataset.name === 'color') {
+      //   ctx.fillStyle = editor['current-color'];
+      //   ctx.strokeStyle = editor['current-color'];
+      // } else {
+      //   ctx.fillStyle = ctx.fillStyle;
+      // }
     }
   },
   startDraw() {
@@ -74,7 +84,6 @@ let editor = {
       ctx.moveTo(editor.x, editor.y);
       ctx.beginPath();
       ctx.ellipse(editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI / 4, 0, 2 * Math.PI);
-      // ctx.stroke();
       ctx.fill();
       ctx.closePath();
     };
@@ -85,21 +94,34 @@ let editor = {
       ctx.beginPath();
       ctx.ellipse(editor.x, editor.y, editor['current-size'], editor['current-size'], Math.PI / 4, 0, 2 * Math.PI);
       ctx.stroke();
-      // ctx.fill();
       ctx.closePath();
     };
   },
   drawSpray() {
     canvas.onmousemove = () => {
-      ctx.fillRect(editor.x + 10, editor.y + 7, 1, 1);
-      ctx.fillRect(editor.x + 3, editor.y + 9, 1, 1);
-      ctx.fillRect(editor.x, editor.y, 1, 1);
-      ctx.fillRect(editor.x + 7, editor.y + 2, 1, 1);
-      ctx.fillRect(editor.x - 9, editor.y - 5, 1, 1);
-      ctx.fillRect(editor.x - 5, editor.y - 3, 1, 1);
-      ctx.fillRect(editor.x - 2, editor.y - 1, 1, 1);
+      let i = 0;
+      while (i < 10) {
+        let delta = Math.floor(Math.random() * 30);
+        let delta2 = -Math.floor(Math.random() * 30);
+        ctx.fillRect(editor.x + delta, editor.y + delta, 1, 1);
+        ctx.fillRect(editor.x + delta2, editor.y + delta2, 1, 1);
+        i++;
+      }
     };
   },
+  clearCanvas() {
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(2, 2, 996, 696);
+    ctx.fillStyle = '#000';
+  }
 };
 
 editor.init();
+editor.clearCanvas();
+
+
+function update(picker) {
+  editor['current-color'] = picker.toHEXString();
+  ctx.fillStyle = editor['current-color'];
+  ctx.strokeStyle = editor['current-color'];
+}
